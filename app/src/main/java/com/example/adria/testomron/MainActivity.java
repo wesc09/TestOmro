6c259d49c4189f6285d11aa83eba0b1530bf6645
Zane <humbert_qm@hotmail.com>
2017-04-15 17:04:51 +0200

    No retorna el "number"

--- app/src/main/java/com/example/adria/testomron/MainActivity.java
+++ app/src/main/java/com/example/adria/testomron/MainActivity.java
@@ -5,9 +5,6 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.support.annotation.IntegerRes;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -15,14 +12,16 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
-import static android.R.attr.data;
-import static android.R.attr.showText;
+
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static String TAG = MainActivity.class.getSimpleName() + ".java";
     private Button mButton, mButton2, mButton3;
+    private EditText etNombre;
+    static final int PICK_CONTACT_REQUEST = 0;
 
     @Override
 
@@ -31,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        etNombre = (EditText)findViewById(R.id.etNombre);
         mButton = (Button) findViewById(R.id.action_button);
         mButton2 = (Button) findViewById(R.id.action_button2);
         mButton3 = (Button) findViewById(R.id.action_button3);
@@ -82,9 +82,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         return super.onOptionsItemSelected(item);
     }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        int PICK_CONTACT_REQUEST = 1;
         // Check which request it is that we're responding to
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == PICK_CONTACT_REQUEST) {
             // Make sure the request was successful
             if (resultCode == RESULT_OK) {
@@ -106,15 +107,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
                 String number = cursor.getString(column);
 
-                //No se com mostrar el numero de telefon que pilla daqui, alguna idea?
-                int num= Integer.parseInt(number);
-                TextView tv = (TextView)findViewById(R.id.phone_text);
-                tv.setText(num);
-
                 // Do something with the phone number...
+                etNombre.setText(number);
             }
+
         }
     }
+
+
     @Override
     public void onClick(View v) {
         int PICK_CONTACT_REQUEST = 1;
@@ -132,6 +132,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
             intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
             startActivityForResult(intent, PICK_CONTACT_REQUEST);
+
             }
 
 
